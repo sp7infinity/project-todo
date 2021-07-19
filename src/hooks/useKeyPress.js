cf3cbc356d849414c82aa9f9337edd9ba33d0948
sp7infinity <pandasonu589@gmail.com>
2021-07-19 21:02:09 +0530

    Added styling to the platform

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -2,21 +2,21 @@ import { useState, useEffect } from "react";
 
 function useKeyPress(targetKey) {
  
-  const [keyPressed, setKeyPressed] = useState(false);
-
-  function downHandler({ key }) {
-    if (key === targetKey) {
-      setKeyPressed(true);
-    }
-  }
-
-  const upHandler = ({ key }) => {
-    if (key === targetKey) {
-      setKeyPressed(false);
-    }
-  };
+  const [keyPressed, setKeyPressed] = useState(false);  
 
   useEffect(() => {
+    const downHandler = ({ key }) => {
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    }
+  
+    const upHandler = ({ key }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    };
+
     window.addEventListener("keydown", downHandler);
     window.addEventListener("keyup", upHandler);
 

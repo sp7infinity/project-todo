9dba663be5826b5fc9273a5a92470718a7be9f35
sp7infinity <pandasonu589@gmail.com>
2021-07-19 19:10:47 +0530

    Added the todo component

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import { Edit, CheckSquare, XSquare } from 'react-feather';
+import useKeypress from "../../hooks/useKeyPress";
+
+import './Todo.css'
+
+function Todo(props) {
+  
+  const { data, areaKey, index, change, remove } = props;
+
+  const [isInputActive, setIsInputActive] = useState(false);
+  const [inputValue, setInputValue] = useState(data.text);
+
+  const inputRef = useRef(null);
+
+  const enter = useKeypress("Enter");
+  const esc = useKeypress("Escape");
+
+  const onEnter = useCallback(() => {
+    if (enter) {
+      change(areaKey, index, inputValue);
+      setIsInputActive(false);
+    }
+  }, [enter, inputValue, change]);
+
+  const onEsc = useCallback(() => {
+    if (esc) {
+      setInputValue(data.text);
+      setIsInputActive(false);
+    }
+  }, [esc, data.text]);
+
+  
+  useEffect(() => {
+    if (isInputActive) {
+      inputRef.current.focus();
+    }
+  }, [isInputActive]);
+
+  useEffect(() => {
+    if (isInputActive) {
+      onEnter(); 
+      onEsc(); 
+    }
+  }, [onEnter, onEsc, isInputActive]); 
+
+  const handleInputChange = useCallback(
+    (event) => {
+ 
+      setInputValue(event.target.value);
+    },
+    [setInputValue]
+  );
+
+  const handleSave = () => {
+    change(areaKey, index, inputValue);
+    setIsInputActive(false);
+  }
+
+  const handleEdit = () => {
+    setIsInputActive(true);
+  }
+
+  return(
+    <Draggable key={data.id} draggableId={data.id} index={index}>
+      {(provided, snapshot) => 
+        <div
+          className={`todo ${snapshot.isDragging ? " active" : ""} ${isInputActive ? " active": ""}`}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className="text-section">
+            { isInputActive ? 
+              <textarea
+                className="text-input"
+                ref={inputRef}
+                value={inputValue}
+                onChange={handleInputChange}
+              />
+              :
+              <span className="text-input">
+                {data.text}
+              </span>
+            }  
+          </div>
+          <div className="button-section">
+            { isInputActive ?
+              <button className="action-button save-button" onClick={handleSave}><CheckSquare/></button>
+            :
+              <button className="action-button edit-button" onClick={handleEdit}><Edit/></button>
+            }
+            <button className="action-button remove-button" onClick={() => remove(areaKey, index)}><XSquare /></button>
+          </div>
+        </div>
+      }
+    </Draggable>
+  );
+}
+
+export default Todo;
